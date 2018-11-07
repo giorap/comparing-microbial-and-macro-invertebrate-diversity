446d1549a30d8a742783af78daa771b2b81e3069
Giovanni Rapacciuolo <giorapac@gmail.com>
2018-11-07 12:02:59 -0800

    Added code to load/install packages within 2-data-processing.R
    Added code to load/install packages within 2-data-processing.R

--- code/2-processing-data.R
+++ code/2-processing-data.R
@@ -3,6 +3,19 @@
 ###################################################
 ################# DATA PROCESSING #################
 ###################################################
+###############################
+##### -- Load packages -- #####
+###############################
+my_packages <- c("plyr", "dplyr", "magrittr", "XLConnect", "scales", ### data manipulation
+                 "SpadeR", "vegan", "SYNCSA", "iNEXT", "MuMIn", ### algorithms
+                 "RColorBrewer", "ggplot2", "ggpubr", "GGally" ### visualizations
+)
+#### Check for packages that are not already installed and isntall them
+new_packages <- my_packages[!(my_packages %in% installed.packages()[,"Package"])]
+if(length(new_packages)) install.packages(new_packages)
+#### Load all packages
+lapply(my_packages, require, character.only=T)
+
 #####################################
 ##### -- Load microbial data -- #####
 #####################################
